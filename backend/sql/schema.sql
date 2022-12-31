56fa494858c46512d48c364b541ffcee6adc7fb8
Brandon Minjares <brandon.minjares@gmail.com>
2022-12-30 21:58:01 -0800

    Added login and register functions in backend and openapi for them

--- backend/sql/schema.sql
+++ backend/sql/schema.sql
@@ -3,3 +3,7 @@ DROP TABLE IF EXISTS dummy;
 CREATE TABLE dummy(created TIMESTAMP WITH TIME ZONE);
 
 -- Your database schema goes here --
+DROP TABLE IF EXISTS person CASCADE;
+
+CREATE TABLE dummy(created TIMESTAMP WITH TIME ZONE);
+CREATE TABLE person(userID UUID UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(), data jsonb);
